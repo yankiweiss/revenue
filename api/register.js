608f6bcd8487bef40a9213962453af04121f86cd
yankiweiss <yakovw2706@gmail.com>
2025-12-17 11:08:35 -0500

    update

--- api/register.js
+++ api/register.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const registerRouter = express.Router();
-import { handleNewUser, handleSingIns, getAllCurrentUsers } from './controllers/registerController';
+import { handleNewUser, handleSingIns, getAllCurrentUsers } from './controllers/registerController.js';
 
 
 
