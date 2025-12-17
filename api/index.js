761d61f3a9cd75b4bc2fd95e9cbfc29b7d5ebd0f
yankiweiss <yakovw2706@gmail.com>
2025-12-17 09:26:06 -0500

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import patientsRouter from './routes/api/patients.js'
 import cors from 'cors'
 import registerRouter from "./routes/register.js";
@@ -9,10 +7,8 @@ import registerRouter from "./routes/register.js";
 dotenv.config();
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3500;
+
 
 
 app.use(express.json())
