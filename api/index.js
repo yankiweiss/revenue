732b94b6f9dd76a883b1b8d9950911abf525ddf0
yankiweiss <yakovw2706@gmail.com>
2025-12-17 09:22:17 -0500

    update

--- api/index.js
+++ api/index.js
@@ -7,30 +7,24 @@ import cors from 'cors'
 import registerRouter from "./routes/register.js";
 
 dotenv.config();
+const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3500;
-const app = express();
+
 
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, "client", "src")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'src', 'index.js'))
-})
+
 
 app.use('/api/register', registerRouter)
-app.use('/patients', patientsRouter);
+app.use('/api/patients', patientsRouter);
 
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
-  });
-}
+
 
 export default (req, res) => app(req, res);
