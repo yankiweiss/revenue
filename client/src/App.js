d94440248652c5db0571a773bd75fc5b976d3b6d
yankiweiss <yakovw2706@gmail.com>
2025-12-17 15:12:59 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 function App() {
 
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   return (
     <>
