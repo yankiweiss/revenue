957ad601f999c1db237af135f8ee72ab555e04a8
yankiweiss <yakovw2706@gmail.com>
2025-12-29 11:44:45 -0500

    update

--- api/controllers/PatientsController.js
+++ api/controllers/PatientsController.js
@@ -14,7 +14,7 @@ const getAllPatients = async (req, res) => {
         member_id,
         TO_CHAR(worked_date, 'MM/DD/YYYY') AS worked_date,
         TO_CHAR(dob, 'MM/DD/YYYY') AS dob,
-        notes,
+        notes
          FROM patients`;
 
         const results = await dataBasePool.query(getAllData);
